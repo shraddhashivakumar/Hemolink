50dabb16e50c35216e9eca41d6e3b1ea50bf49fc
shraddhashivakumar <162862002+shraddhashivakumar@users.noreply.github.com>
2026-03-23 09:35:29 +0530

    Add files via upload

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "hemolink"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database WOOOHOOOOOOOOO!");
+  }
+});
+
+module.exports = db;
